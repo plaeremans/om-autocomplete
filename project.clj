fffdca677dddecf940ccf064b37c92c2b43ef82a
Pieter Laeremans <pieter@laeremans.org>
2014-10-20 22:08:11 +0200

    bumped to 0.8.0-alpha of om

--- project.clj
+++ project.clj
@@ -7,9 +7,9 @@
   :source-paths ["src"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2277" :scope "provided"]
-                 [org.clojure/core.async "0.1.301.0-deb34a-alpha" :scope "provided"]
-                 [om "0.7.1" :scope "provided"]]
+                 [org.clojure/clojurescript "0.0-2356" :scope "provided"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha" :scope "provided"]
+                 [om "0.8.0-alpha1" :scope "provided"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
 
